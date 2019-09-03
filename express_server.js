9497d3e119bc636548808f7485e0611a955d73e2
kirillradaev <cyrilrdv@gmail.com>
2019-09-03 00:17:14 +0000

    finished URL shortening part 1

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 
@@ -30,8 +33,21 @@ app.get('/urls', (req, res) => {
    res.render('urls_index', templateVars);
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new')
+});
+
 app.get('/urls/:shortURL', (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   console.log(templateVars);
   res.render('urls_show', templateVars);
 });
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('Ok');
+});
+
+function generateRandomString () {
+  return Math.random().toString(36).substring(2, 5) + Math.random().toString(36).substring(2, 5);
+}
